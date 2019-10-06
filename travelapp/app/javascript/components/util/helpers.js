754bf0f3a59bacf62c7269aa45dcb97b38e6231e
Ryan Kortmann <rkortmann@jackthreads.com>
2019-10-05 23:47:47 -0400

    New safeGet function, now null safe

--- travelapp/app/javascript/components/util/helpers.js
+++ travelapp/app/javascript/components/util/helpers.js
@@ -1,20 +1,9 @@
 // Safe get
-export function safeGet(obj, evalFunc, def) {
-  // Our proxy handler
-  const handler = {
-    // Intercept all property access
-    get: function(target, prop, receiver) {
-      const res = Reflect.get(...arguments);
-
-      // If our response is an object then wrap it in a proxy else just return
-      return typeof res === "object" ? proxify(res) : res != null ? res : def;
-    }
-  };
-
-  const proxify = target => {
-    return new Proxy(target, handler);
-  };
-
-  // Call function with our proxified object
-  return evalFunc(proxify(obj, handler));
+export function safeGet(func, fallbackValue) {
+  try {
+    var value = func();
+    return (value === null || value === undefined) ? fallbackValue : value;
+  } catch (e) {
+    return fallbackValue;
+  }
 }
